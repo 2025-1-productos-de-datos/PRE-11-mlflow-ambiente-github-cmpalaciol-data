723169916dce56aa33e58bee58613e76e2f65b9c
cmpalaciol-data <cmpalaciol@unal.edu.co>
2025-05-31 10:13:32 -0500

    Update test_homework.py

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -2,4 +2,4 @@ import os
 
 
 def test_01():
-    assert os.path.exists("MLproject.txt"), "El archivo MLproject.txt no existe."
+    assert os.path.exists("MLproject1.txt"), "El archivo MLproject.txt no existe."
